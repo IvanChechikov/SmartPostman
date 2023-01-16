1d530f0655a9eab48ef0f4a12516d12439bbcfe6
Coder69 <122150903+IvanChechikov@users.noreply.github.com>
2023-01-16 11:03:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-import traceback
-import sys
-import logging
-from bottle import default_app, request, post
+from bottle import request, post, default_app
 from mail_tools import get_sender_email_data, send_email, get_recipient_email_text, get_email_obj_text, \
     get_email_address_list
 import json
 import re
 
-response_texts = []
+response_texts  = []
 response_ttss = []
 
 
@@ -69,20 +66,18 @@ def work():
                                 sender_email_name = get_sender_email_data(access_token)[0]
                                 global sender_email
                                 sender_email = get_sender_email_data(access_token)[1]
-                                response["response"]["text"] = f"Получаю ваш текущий email, ваш" \
-                                                               f" email - {sender_email}. Чтобы продолжить," \
-                                                               f" ответьте, 'список контактов'. "
-                                response["response"]["tts"] = f" Получ+аю ваш тек+ущий " \
-                                                              f" email, ваш email - {sender_email}. " \
+                                response["response"]["text"] = f"{sender_email_name}, идентификация вашего email завершена." \
+                                                              f" Чтобы продолжить, ответьте, 'список контактов'." 
+                                response["response"]["tts"] = f"{sender_email_name}, идентифик+ация в+ашего email завершен+а." \
                                                               f" Чт+обы прод+олжить отв+етьте сп+исок конт+актов."
                                 response_texts.append(response["response"]["text"])
                                 response_ttss.append(response["response"]["tts"])
                             else:
-                                response["response"]["text"] = "Не удалось определить ваш email. Что то пошло не так," \
+                                response["response"]["text"] = "Не удалось определить ваши почтовые данные. Есть проблема," \
                                                                " начните сначала. Ответьте 'старт' или 'помощь'."
 
-                                response["response"]["tts"] = "Не удал+ось определ+ить ваш email." \
-                                                              " Что то пошл+о не так, начн+ите снач+ала. " \
+                                response["response"]["tts"] = "Не удал+ось определ+ить в+аши почт+овые д+анные." \
+                                                              " Есть пробл+ема, начн+ите снач+ала. " \
                                                               " Отв+етьте старт или п+омощь. "
                                 response_texts.append(response["response"]["text"])
                                 response_ttss.append(response["response"]["tts"])
@@ -93,14 +88,14 @@ def work():
                             if email_address_list:
                                 response["response"]["text"] = "Список контактов сформирован." \
                                                                " Я могу начать писать письмо. Кому будем отправлять? " \
-                                                               " Назовите получателя, сказав 'получатель'," \
+                                                               " Назовите получателя, ответив 'получатель'," \
                                                                " а дальше назовите его имя и фамилию," \
                                                                " если это физ. лицо," \
                                                                " или же название компании, если это юр. лицо"
 
                                 response["response"]["tts"] = "Сп+исок конт+актов сформир+ован. Я мог+у нач+ать пис+ать письм+о." \
                                                               " Ком+у б+удем отправл+ять? Назов+ите получ+ателя," \
-                                                              " сказ+ав получ+атель, а д+альше назов+ите ег+о имя" \
+                                                              " отв+етив получ+атель, а д+альше назов+ите ег+о имя" \
                                                               " и фам+илию, если это физ лиц+о или же назв+ание" \
                                                               " комп+ании, если это юр лиц+о. "
                                 response_texts.append(response["response"]["text"])
@@ -123,28 +118,28 @@ def work():
                                 response["response"]["text"] = f"Email получателя {recipient_email}. Чтобы продолжить," \
                                                                f" ответьте, 'тема письма'." \
                                                                f" Если хотите изменить email получателя," \
-                                                               f" введите прошлую команду еще раз."
+                                                               f" ответьте прошлую команду еще раз."
                                 response["response"]["tts"] = f"Email получ+ателя {recipient_email}." \
                                                               f" Чт+обы прод+олжить, отв+етьте, т+ема письм+а." \
                                                               f" Если хот+ите измен+ить email получ+ателя," \
-                                                              f" введ+ите пр+ошлую ком+анду ещ+е раз."
+                                                              f" отв+етьте пр+ошлую ком+анду ещ+е раз."
                                 response_texts.append(response["response"]["text"])
                                 response_ttss.append(response["response"]["tts"])
                             else:
                                 response["response"]["text"] = "Не удалось определить email получателя." \
                                                                " Возможно его нет в сформированном списке контактов." \
-                                                               " Попробуйте снова, введите прошлую команду еще раз."
+                                                               " Попробуйте снова, ответьте прошлую команду еще раз."
                                 response["response"]["tts"] = "Не удал+ось определ+ить email получ+а" \
                                                               " теля. Возм+ожно его нет в сформир+ованном" \
                                                               " сп+иске конт+актов. Попр+обуйте сн+ова," \
-                                                              " введ+ите пр+ошлую ком+анду еще раз."
+                                                              " отв+етьте пр+ошлую ком+анду еще раз."
                                 response_texts.append(response["response"]["text"])
                                 response_ttss.append(response["response"]["tts"])
 
                         elif req["request"]["original_utterance"].lower().strip() == "тема письма":
-                            response["response"]["text"] = "Придумайте тему письма, сказав, 'тема'...," \
+                            response["response"]["text"] = "Придумайте тему письма, ответив, 'тема'...," \
                                                            " а дальше только полёт вашей мысли"
-                            response["response"]["tts"] = "Прид+умайте т+ему письм+а, сказав, т+ема...," \
+                            response["response"]["tts"] = "Прид+умайте т+ему письм+а, отв+етив, т+ема...," \
                                                           " а д+альше т+олько пол+ёт в+ашей м+ысли"
                             response_texts.append(response["response"]["text"])
                             response_ttss.append(response["response"]["tts"])
@@ -157,18 +152,18 @@ def work():
                             response["response"]["text"] = f"Тема письма - {subject} Чтобы продолжить," \
                                                            f" ответьте, 'текст письма'." \
                                                            f" Если хотите изменить тему письма," \
-                                                           f" скажите 'тема письма' еще раз."
+                                                           f" ответьте прошлую команду еще раз."
                             response["response"]["tts"] = f"Т+ема письм+а - {subject} Чт+обы прод+олжить," \
                                                           f" отв+етьте, текст письм+а." \
                                                           f" Если хот+ите измен+ить т+ему письм+а, " \
-                                                          f" скаж+ите т+ема письм+а ещ+е раз."
+                                                          f" отв+етьте пр+ошлую ком+анду еще раз."
                             response_texts.append(response["response"]["text"])
                             response_ttss.append(response["response"]["tts"])
 
                         elif req["request"]["original_utterance"].lower().strip() == "текст письма":
-                            response["response"]["text"] = "Придумайте текст письма, сказав, 'текст'...," \
+                            response["response"]["text"] = "Придумайте текст письма, ответив, 'текст'...," \
                                                            " а дальше только полёт вашей мысли"
-                            response["response"]["tts"] = "Прид+умайте текст письм+а, сказ+ав, текст..." \
+                            response["response"]["tts"] = "Прид+умайте текст письм+а, отв+етив, текст..." \
                                                           " а д+альше т+олько пол+ёт в+ашей м+ысли"
                             response_texts.append(response["response"]["text"])
                             response_ttss.append(response["response"]["tts"])
@@ -181,11 +176,11 @@ def work():
                             response["response"]["text"] = f"Текст письма - {message} Чтобы продолжить," \
                                                            f" ответьте, 'отправка письма'." \
                                                            f" Если хотите изменить текст письма," \
-                                                           f" скажите 'текст письма' еще раз."
+                                                           f" ответьте прошлую команду еще раз."
                             response["response"]["tts"] = f"Текст письм+а - {message} Чт+обы прод+олжить," \
                                                           f" отв+етьте, отпр+авка письм+а." \
                                                           f" Если хот+ите измен+ить текст письм+а, " \
-                                                          f" скаж+ите текст письм+а ещ+е раз."
+                                                          f" отв+етьте пр+ошлую ком+анду еще раз."
                             response_texts.append(response["response"]["text"])
                             response_ttss.append(response["response"]["tts"])
 
